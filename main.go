9c32756edf79da9aacd71c139a6768000d2087d6
jutinapas <jutinapas@gmail.com>
2021-09-07 09:34:53 +0700

    complete a simple webservice

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/jutinapas/webservice/controllers"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	port := 3000
+	// fmt.Println("Hello, World!")
 
-	port, err := startWebServer(port, 2)
-	fmt.Println(port, err)
+	// port := 3000
+	// port, err := startWebServer(port, 2)
+	// fmt.Println(port, err)
+
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
 
-// data type of arguments can be defined as a comma-delimited
-// multiple return values is allowed in Go
-func startWebServer(port, numberOfRetries int) (int, error) {
-	fmt.Println("starting server...")
-	fmt.Println("starting started on port", port)
-	fmt.Println("number of retries", numberOfRetries)
+// // data type of arguments can be defined as a comma-delimited
+// // multiple return values is allowed in Go
+// func startWebServer(port, numberOfRetries int) (int, error) {
+// 	fmt.Println("starting server...")
+// 	fmt.Println("starting started on port", port)
+// 	fmt.Println("number of retries", numberOfRetries)
 
-	// error is a pointer data type
-	return port, nil
-}
+// 	// error is a pointer data type
+// 	return port, nil
+// }
